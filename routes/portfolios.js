376b35c642259c0245316f9042acaec5e3552593
Jim Lusk <jblusk@gmail.com>
2020-10-03 09:35:25 -0700

    Serve mock data from portfolios route

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var mockjson = require('../data/positions.json');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('placeholder for portfolios api');
+  //TODO Add handling for dynamic variables
+  //TODO Add handling for mock variable id 
+  res.json(mockjson);
 });
 
 module.exports = router;
